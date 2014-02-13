c0866a2aa1e24634acf8842445343b236d4ecacb
dsbmac <dsbmac@G580.(none)>
2014-02-12 22:31:14 -0500

    Initial Lab 5 Intents commit

---
+++ LoginScreen/gen/course/examples/helloWorldWithLogin/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package course.examples.helloWorldWithLogin;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
